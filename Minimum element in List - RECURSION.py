f717a02c21b53951593dc1ac2961e025b80cef75
navyasrikarri <141988192+navyasrikarri@users.noreply.github.com>
2024-06-03 14:30:23 +0530

    Create Minimum element in List - RECURSION.py

---
+++ Minimum element in List - RECURSION.py
@@ -0,0 +1,8 @@
+def find_max(index,n,nums):
+  if index == n-1:
+    return nums[index] 
+  maxi = find_max(index+1,n,nums) 
+  return min(maxi,nums[index])  
+nums = [1,2,3,4,5] 
+n = len(nums) 
+print(find_max(0,n,nums)) 
