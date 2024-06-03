32bfe16ef29bca63746426147fd3d8f845b69343
navyasrikarri <141988192+navyasrikarri@users.noreply.github.com>
2024-06-03 15:05:42 +0530

    Vowels count in string - RECURSION.py

--- Vowels count in string - RECURSION.py
+++ Vowels count in string - RECURSION.py
@@ -1,10 +1,29 @@
-def func(index, n, string, c):
+def countVowelsWay1(word, index, n, result):
     if index == n:
-        return c 
-    if string[index] in "aeiou":
-        c += 1 
-    func(index + 1, n, string, c) 
-
-string = "aei"  
-c = 0
-print(func(0, 3, string, c))
+        print("Vowels count is:", result)
+        return 
+    vowels = "aeiouAEIOU"
+    if word[index] in vowels:
+        result += 1 
+    countVowelsWay1(word, index + 1, n, result)
+ 
+ 
+word = "abcdeefuigh"
+countVowelsWay1(word, 0, len(word), 0)
+ 
+ 
+ 
+ 
+def countVowelsWay2(word, index, n):
+    if index == n:
+        return 0 
+    nextVowelsCount = countVowelsWay2(word, index + 1, n)
+    vowels = "aeiouAEIOU"
+    if word[index] in vowels:
+        nextVowelsCount += 1 
+    return nextVowelsCount
+ 
+ 
+word = "abcdeefuigh"
+result = countVowelsWay2(word, 0, len(word))
+print("Vowels count is:", result)
